e3f9c8ebc155e1cb83bb9abf03f4c515d1234fa9
Kyano DM <81612475+KyanoBoi@users.noreply.github.com>
2023-05-15 14:19:49 +0200

    Add files via upload

---
+++ Huur/js/nav.js
@@ -0,0 +1,17 @@
+const navEL = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 70) {
+        navEL.classList.add('navbar-scrolled');
+        navLinks.forEach(link => link.classList.add('col-white'));
+    } else {
+        navEL.classList.remove('navbar-scrolled');
+        navLinks.forEach(link => link.classList.remove('col-white'));
+    }
+
+    if (window.scrollY > 950) {
+        navEL.classList.remove('navbar-scrolled');
+        navLinks.forEach(link => link.classList.remove('col-white'));
+    }
+});
